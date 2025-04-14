6ebc763cfa3db3b9cde008605b02276a3b1262d9
Fábio <otymus27@gmail.com>
2025-04-14 11:56:20 -0300

    Primeiro commit - iniciado projeto com estrutura básica

---
+++ src/main/java/otymus/com/apiremedio/ApiRemedioApplication.java
@@ -0,0 +1,13 @@
+package otymus.com.apiremedio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRemedioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRemedioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/otymus/com/apiremedio/ApiRemedioApplicationTests.java
@@ -0,0 +1,13 @@
+package otymus.com.apiremedio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRemedioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
